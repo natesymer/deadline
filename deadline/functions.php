6b0dbca0973c4cf18b980867f64587bf974f61dd
Nate Symer <nate@symer.io>
2023-09-19 14:49:08 -0400

    Write TODO

--- deadline/functions.php
+++ deadline/functions.php
@@ -1,12 +1,12 @@
 <?php
 
-/**
- * Sets up theme defaults and registers support for various WordPress features.
- *
- * Note that this function is hooked into the after_setup_theme hook, which
- * runs before the init hook. The init hook is too late for some features, such
- * as indicating support for post thumbnails.
- */
+/*
+	TODO:
+	1. Figure out page headers
+	2. Figure out what the base CSS should be
+		- We don't want it too extensive
+*/
+
 add_action('after_setup_theme', function() {
 	add_theme_support('title-tag');
 	add_theme_support('post-thumbnails');
