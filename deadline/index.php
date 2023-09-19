dcceb89f4a2310aaa0e1049559406cdf031759ac
Nate Symer <nate@symer.io>
2023-09-19 12:21:58 -0400

    Disable deploy

--- deadline/index.php
+++ deadline/index.php
@@ -1,16 +1,4 @@
 <?php
-/**
- * The main template file
- *
- * This is the most generic template file in a WordPress theme
- * and one of the two required files for a theme (the other being style.css).
- * It is used to display a page when nothing more specific matches a query.
- * E.g., it puts together the home page when no home.php file exists.
- *
- * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
- *
- * @package deadline
- */
 
 get_header();
 ?>
@@ -25,21 +13,14 @@ get_header();
 				<?php
 			}
 
-			/* Start the Loop */
 			while (have_posts()) {
 				the_post();
-
-				/*
-				 * Include the Post-Type-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
-				 */
 				get_template_part( 'template-parts/content', get_post_type() );
 			}
 
 			the_posts_navigation();
 		} else {
-			get_template_part( 'template-parts/content', 'none' );
+			get_template_part('template-parts/content', 'none');
 		}
 		?>
 
